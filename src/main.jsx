d8eeaafe6140ea4c5950d3b443afe5511c604f1c
Roesn <Bubwub04@icloud.com>
2023-12-02 20:58:49 -0500

    cleaning

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
+// import lib
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 
+// mount app to main
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
