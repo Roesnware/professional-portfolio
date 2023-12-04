c1fa4b097608b95d568e1083cd89af9816661dee
Roesn <Bubwub04@icloud.com>
2023-12-04 12:34:19 -0500

    home container

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,9 +1,15 @@
 import './Home.css'
 
 const Home = () => {
+    const text = 'Im a web developer';
     return (
-        <div className='avatar'>
-            <div className='itachi'></div>
+        <div className='content'>
+            <div className='avatar'>
+                <div className='itachi'></div>
+            </div>
+            <div className='introText'>
+                <h1>Hi my name is <span className='name'>Trae</span></h1>
+            </div>
         </div>
     );
 }
