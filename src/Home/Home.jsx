6b4581328c79d8ae2ce1dad0402410f0610696b6
Roesn <Bubwub04@icloud.com>
2023-12-06 14:48:06 -0500

    blur mist, typewriter

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,15 +1,38 @@
 import './Home.css'
+import { useTypewriter, Cursor } from 'react-simple-typewriter'
 
 const Home = () => {
-    const text = 'Im a web developer';
+    const start = '{ '
+    const end = ' }'
+
+    const [typeEffect] = useTypewriter({
+        words: ['web developer', 'problem solver', 'anime enthusiast', 'team player'],
+        loop: {},
+        typeSpeed: 100,
+        deleteSpeed: 40
+    })
     return (
-        <div className='content'>
-            <div className='avatar'>
-                <div className='itachi'></div>
+        <div>
+            <div className='blur blur1'></div>
+            <div className='blur blur3'></div>
+            <div className='content'>
+                <div className='avatar'>
+                    <div className='memoji'></div>
+                </div>
+                <div className='introText container'>
+                    <div className='curly'>
+                        {start}
+                    </div>
+                    <div className='text'>
+                        <h1>Hi my name is <span className='name'>Trae</span></h1>
+                        <h1>Im a <span className='typewriter'>{typeEffect}</span></h1>
+                    </div>
+                    <div className='curly'>
+                        {end}
+                    </div>
+                </div>
             </div>
-            <div className='introText'>
-                <h1>Hi my name is <span className='name'>Trae</span></h1>
-            </div>
+            <div className='blur blur2'></div>
         </div>
     );
 }
