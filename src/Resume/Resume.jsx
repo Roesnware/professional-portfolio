f5fd509aee2407533fc5191940cfa209c5676dad
Roesn <Bubwub04@icloud.com>
2023-12-06 20:56:04 -0500

    blur added

--- src/Resume/Resume.jsx
+++ src/Resume/Resume.jsx
@@ -6,6 +6,9 @@ const Resume = () => {
     //return 
     return (
         <div className='Resume'>
+            <div className='blur blur9'></div>
+            <div className='blur blur10'></div>
+            <div className='blur blur11'></div>
             <h1><a href="" target='_blank'>Download My Resume Here!</a></h1>
         </div>
     );
