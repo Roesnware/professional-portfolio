cad134ba87f1b802cdea3059c00638c4e0a63b6f
Roesn <Bubwub04@icloud.com>
2023-12-06 20:43:46 -0500

    form response

--- src/ContactMe/ContactMe.jsx
+++ src/ContactMe/ContactMe.jsx
@@ -4,6 +4,24 @@ import './ContactMe.css'
 // contact component
 const ContactMe = () => {
 
+    const handleFormSubmit = async () => {
+
+        const emailHook = document.getElementById('email');
+        const messageHook = document.getElementById('message');
+
+        const email = emailHook.value.trim();
+        const message = messageHook.value.trim();
+
+        const submitBtn = document.getElementById('submitBtn');
+
+        const res = await console.log(email, message);
+        submitBtn.innerText = "Submitted!";
+        submitBtn.style.backgroundColor = 'red';
+
+        emailHook.value = '';
+        messageHook.value = '';
+    }
+
     // return 
     return (
         <div className='formCont'>
@@ -12,12 +30,12 @@ const ContactMe = () => {
             <div className='blur blur8'></div>
             <div className='formInput'>
                 <p>Email: </p>
-                <input type="text" name='email' id='email' placeholder='eg. traeroy@email.com'/>
-                <br/>
+                <input type="text" name='email' id='email' placeholder='eg. traeroy@email.com' />
+                <br />
                 <p>Message: </p>
-                <input type="text" name='message' id='message' placeholder='eg. type your message here'/>
-                <br/>
-                <button onClick={() => {handleFormSubmit}}>Submit</button>
+                <input type="text" name='message' id='message' placeholder='eg. type your message here' />
+                <br />
+                <button id="submitBtn" onClick={() => { handleFormSubmit() }}>Submit</button>
             </div>
         </div>
     );
