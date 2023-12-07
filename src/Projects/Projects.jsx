5c4cb3fb9501661eb7d68707098e6d9af3d60f1c
Roesn <Bubwub04@icloud.com>
2023-12-06 21:05:40 -0500

    project page temp

---
+++ src/Projects/Projects.jsx
@@ -0,0 +1,28 @@
+// import stlying 
+import './Projects.css'
+
+// project component
+const Projects = () => {
+    // return 
+    return (
+        <div className='Projects'>
+            <div className='ProjCont'>
+                <div className='Project'>
+                    <div className='ProjDemo'>
+                        <div>
+                            <img src="" alt="" />
+                        </div>
+                        <div className='ProjTech'></div>
+                    </div>
+                    <div className='ProjDetails'>
+                        <a href="" target='_blank'>Source Code</a>
+                        <a href="" target='_blank'>Live Site</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+// export projects 
+export default Projects;
